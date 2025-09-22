00eac12befb1e6747472b2dab33182b645447a71
gkaushik12 <gauravkaushik12061999@gmail.com>
2025-09-22 11:47:19 +0530

    Project 40 done

---
+++ P40 - 3D Background Boxes/script.js
@@ -0,0 +1,27 @@
+const boxesContainer = document.getElementById("boxes");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () =>
+  boxesContainer.classList.toggle("big")
+);
+
+btn.addEventListener("click", () => {
+  if (boxesContainer.classList.contains("big")) {
+    btn.innerHTML = "Merge";
+  } else {
+    btn.innerHTML = "Separate";
+  }
+});
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxesContainer.appendChild(box);
+    }
+  }
+}
+
+createBoxes();
